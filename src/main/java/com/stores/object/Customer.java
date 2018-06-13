5077f5c4b4ae480854f14b0043979d6c7b013d62
Ashish Prabhu <ashish.pprabhu@gmail.com>
2018-06-14 01:43:23 +0530

    defining the customer Object and reading Queue Information from file

---
+++ src/main/java/com/stores/object/Customer.java
@@ -0,0 +1,34 @@
+package com.stores.object;
+
+public class Customer implements ICustomer {
+	
+	private static final String REC_SEPARATOR = " ";
+	
+	private String customerType;
+	private int timeArrived;
+	private int itemsInHand;
+	
+	public Customer(String customerEntry){
+		String[] records = customerEntry.split(REC_SEPARATOR);
+		this.customerType = records[0];
+		this.timeArrived = Integer.parseInt(records[1]);
+		this.itemsInHand = Integer.parseInt(records[2]);
+	}
+	
+	public String getCustomerType() {
+		return customerType;
+	}
+
+	public int getTimeArrived() {
+		return timeArrived;
+	}
+
+	public int getItemsInHand() {
+		return itemsInHand;
+	}
+	
+	public void billItem(){
+		itemsInHand--;
+	}
+
+}
--- src/main/java/com/stores/starter/CounterStarter.java
+++ src/main/java/com/stores/starter/CounterStarter.java
@@ -1,9 +1,19 @@
 package com.stores.starter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class CounterStarter {
 
-	public static void main(String ...args){
+	public static void main(String ...args) throws Exception{
 		System.out.println("Hello World");
+		String line;
+		
+		BufferedReader inputReader = new BufferedReader(new FileReader("/Users/aprabhu/Documents/workspace/GroceryStoreCounters/grocery-store-counters/src/main/resources/input/input.txt"));
+		
+		while ((line = inputReader.readLine()) != null){
+			System.out.println(line);
+		}
 	}
 	
 }
