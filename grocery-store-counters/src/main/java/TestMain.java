b0abec18e67b613cf9efe7a54799078719225bc3
Ashish Prabhu <ashish.prabhu@travelex.com>
2018-06-14 13:17:58 +0530

    Reorganizing the folder structure

---
+++ grocery-store-counters/src/main/java/TestMain.java
@@ -0,0 +1,9 @@
+
+public class TestMain {
+
+	public static void main(String ...args){
+		String str = "A 1 1";
+		String[] arr = str.split(" ");
+		System.out.println(arr.length);
+	}
+}
---
+++ grocery-store-counters/src/main/java/com/stores/object/ICustomer.java
@@ -0,0 +1,10 @@
+package com.stores.object;
+
+public interface ICustomer {
+	
+	public String getCustomerType();
+	public int getTimeArrived();
+	public int getItemsInHand();
+	public void billItem();
+	
+}
